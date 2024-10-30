b76d39e2e8ff1e5af274eca4ea1015326945ff8c
GeonhyoKoo <ggh1222@naver.com>
2024-10-30 21:33:46 +0900

    초기 환경 세팅

---
+++ src/main/java/com/memo/post/mapper/PostMapper.java
@@ -0,0 +1,16 @@
+package com.memo.post.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface PostMapper {
+
+	// input : x
+	// output : List<Map<String, Object>>
+	public  List<Map<String, Object>> selectPostList();
+	
+	
+}
